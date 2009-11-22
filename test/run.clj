3471541294e9a754d8329f07e50dd6b335a68236
ggironda <gabriel.gironda@centro.net>
2009-11-22 01:59:30 -0600

    actually adding files we need

---
+++ test/run.clj
@@ -0,0 +1,28 @@
+(ns test.run
+  (:use clojure.contrib.test-is)
+  (:require test.compojure.crypto)
+  (:require test.compojure.html.gen)
+  (:require test.compojure.html.form-helpers)
+  (:require test.compojure.html.page-helpers)
+  (:require test.compojure.http.helpers)
+  (:require test.compojure.http.middleware)
+  (:require test.compojure.http.routes)
+  (:require test.compojure.http.request)
+  (:require test.compojure.http.response)
+  (:require test.compojure.http.session)
+  (:require test.compojure.str-utils)
+  (:require test.compojure.validation))
+
+(run-tests
+  'test.compojure.crypto
+  'test.compojure.html.gen
+  'test.compojure.html.form-helpers
+  'test.compojure.html.page-helpers
+  'test.compojure.http.helpers
+  'test.compojure.http.middleware
+  'test.compojure.http.routes
+  'test.compojure.http.request
+  'test.compojure.http.response
+  'test.compojure.http.session
+  'test.compojure.str-utils
+  'test.compojure.validation)
